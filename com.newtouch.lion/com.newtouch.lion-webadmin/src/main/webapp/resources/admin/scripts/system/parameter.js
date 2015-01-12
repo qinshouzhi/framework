1ae65da6b8bff557d265052a85cd1b9b9a55a3ce
scwanglijun <scwanglijun@gmail.com>
2015-01-12 13:50:00 +0800

    修改参数名称

--- com.newtouch.lion/com.newtouch.lion-webadmin/src/main/webapp/resources/admin/scripts/system/parameter.js
+++ com.newtouch.lion/com.newtouch.lion-webadmin/src/main/webapp/resources/admin/scripts/system/parameter.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 	            tickIcon: 'fa-check'
 	        });
 	}
-	
+	//初始化下拉框
 	handleBootstrapSelect();
 
 	$("#sys_parameter_lists_tb").datagrid({
@@ -22,24 +22,34 @@ $(document).ready(function() {
 	 function dataGridReload(dataGridId){
 		$("#"+dataGridId).datagrid("reload");
 	 }
-	 
+	 //刷新
 	 $("#btnRefresh").on("click",function(){
 		 dataGridReload("sys_parameter_lists_tb");
 	 });
-	 var data = [];
-	 $("#sysParameterCodeList option").each(function () {
-		 var row = {};
-         var codeText = $(this).text(); //获取单个text
-         var codeValue = $(this).val(); //获取单个value
-         row.codeText=codeText;
-         row.codeValue=codeValue;
-         data.push(row);
-     });
+	 
+	 $("#btnAdd").on("click",function(){
+		 alert("dd");
+	 });
+	
 });
-
-
+//获取下拉列表数据
+var data = [];
+$("#sysParameterCodeList option").each(function () {
+	var row = {};
+    var codeText = $(this).text(); //获取单个text
+    var codeValue = $(this).val(); //获取单个value
+    row.codeText=codeText;
+    row.codeValue=codeValue;
+    data.push(row);
+});
 /**sys_code_type 加载列表*/
 function formatterCodeList(val,row) {
-	var nameZh = "中文";
-	return nameZh;
+	var codeText="";
+	for ( var obj in data) {
+		if (data[obj].codeValue ===val) {
+			codeText = data[obj].codeText;
+			break;
+		}
+	}
+	return codeText;
 }
