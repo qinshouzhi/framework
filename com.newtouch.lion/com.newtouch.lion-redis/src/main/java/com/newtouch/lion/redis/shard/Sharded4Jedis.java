46087e048a62603e2b927b69fe84426fc27b5d19
Wanglijun <lijun.wang1@newtouch.cn>
2015-03-16 11:06:03 +0800

    Redis缓存工具类

---
+++ com.newtouch.lion/com.newtouch.lion-redis/src/main/java/com/newtouch/lion/redis/shard/Sharded4Jedis.java
@@ -0,0 +1,22 @@
+package com.newtouch.lion.redis.shard;
+
+import java.util.List;
+
+/**
+ * 
+ * 
+ * 功能描述： 包括所有分片的连接池
+ * 
+ */
+public class Sharded4Jedis extends DefaultSharded<ShardHandler, ShardInfo4Jedis> {
+
+    /**
+     * 构造方法
+     * 
+     * @param shards 分片列表
+     */
+    public Sharded4Jedis(List<ShardInfo4Jedis> shards) {
+        super(shards);
+    }
+
+}
